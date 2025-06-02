03a64d762db2db9f229bea9ff8feb91efa33c478
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-06-02 20:03:36 -0300

    nao aguento mais esse semestreeeeee

--- inacio.cpp
+++ inacio.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
-    cout << "hello world";
-    
-    return 0;
+int addRoundKey(int estado_atual, int k0, int k1){
+    int chave = (k0 << 8) | k1; //subchave
+    return estado_atual ^ chave; 
 }
