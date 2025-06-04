03602a1793616b2d0d16b54dfb19d23e4f515529
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-06-04 20:36:37 -0300

    galois field

--- inacio.cpp
+++ inacio.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h>
+#include <bitset>
 using namespace std;
+
+//s-box
+vector<int> sbox{9, 4, 10, 11, 13, 1, 8, 5, 6, 2, 0, 3, 12, 14, 15, 7};
+
+int GF(int a, int b){
+    int resultado = 0;
+    while(b){
+        int formula = 19; //x**4 + x + 1
+
+        if(b & 1)
+            resultado ^= a;
+        a <<= 1;
+        if(a & 16)
+            a ^= 19;
+        
+    }
+}
 
 int addRoundKey(int estado_atual, int k0, int k1){
     int chave = (k0 << 8) | k1; //subchave
