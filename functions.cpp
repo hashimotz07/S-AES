be1a0d82a187b7fcb788192922ee68c2bf3b6e61
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-06-16 01:35:10 -0300

    separando os arquivos

--- functions.cpp
+++ functions.cpp
@@ -5,8 +5,6 @@ using namespace std;
 //s-box
 vector<int> sbox{9, 4, 10, 11, 13, 1, 8, 5, 6, 2, 0, 3, 12, 14, 15, 7};
 
-
-
 int GF(int a, int b){
     int resultado = 0;
     while(b){
@@ -22,7 +20,7 @@ int GF(int a, int b){
     return resultado & 15;
 }
 
-int multiplicaColunas(int estado){
+int mixColumns(int estado){
     int s0 = (estado >> 12) & 15;
     int s1 = (estado >> 8) & 15;
     int s2 = (estado >> 4) & 15;
@@ -80,4 +78,24 @@ int shiftRows(int input){
 int addRoundKey(int estado_atual, int k0, int k1){
     int chave = (k0 << 8) | k1; //subchave
     return estado_atual ^ chave; 
+}
+
+int encripta(int plaintext,  vector<int>& chaves) {
+    int estado = plaintext;
+    
+    //rodada inicial
+    estado ^= chaves[0];
+    
+    //rodada 1
+    estado = subNibble(estado);
+    estado = shiftRows(estado);
+    estado = mixColumns(estado);
+    estado ^= chaves[1];
+    
+    //rodada 2
+    estado = subNibble(estado);
+    estado = shiftRows(estado);
+    estado ^= chaves[2];
+    
+    return estado;
 }
